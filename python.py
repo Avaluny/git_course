bb506fb654175f9c1037f72b59c499d6a7fc68fd
Anastasia Valueva <a.valueva@mail.ru>
2023-12-05 14:41:47 +0300

    Changed python file

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-l = len(a)
-for i in range(l):
-    if a[i]<20: print(a[i])
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+result = list(filter(lambda elem: elem in b, a))
+print(result)
